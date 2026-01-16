e6a0b73f483297a2839bd343079e7a9600d4c863
GitHub Action <action@github.com>
2026-01-16 13:44:14 +0000

    chore: update master script after merge [skip ci]

---
+++ RELEASE_SCRIPTS/Sprint160/Master_Release_Final.sql
@@ -0,0 +1,16 @@
+-- BASELINE FROM ADHOC_SCRIPT.SQL --
+Create or Replace dbo_Schema.table1
+();
+
+Alter table table1
+add id varchar(255);
+
+--alter statement...
+
+
+
+Grants select on Tablename to devrole;
+
+
+-- ####### MERGED CONTENT START #######
+

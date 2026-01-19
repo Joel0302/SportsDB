18f632bee89aed5d7fed8f60cb635a05c338fd60
Joel Mario J <109203750+Joel0302@users.noreply.github.com>
2026-01-19 17:31:17 +0530

    Modify Adhoc_Script.sql to add tables and columns
    Added new table creation and altered existing tables to include ID columns.

--- RELEASE_SCRIPTS/Sprint160/Adhoc_Script.sql
+++ RELEASE_SCRIPTS/Sprint160/Adhoc_Script.sql
@@ -1,10 +1,16 @@
 Create or Replace dbo_Schema.table1
 ();
 
+create or replace tablename1();
+
+
 Alter table table1
 add id varchar(255);
 
---alter statement...
+
+
+Alter table table2
+add id varchar(2000);
 
 
 

babbd1c5faf807e82b167fa89a12ebc03a16cc3d
GitHub Action <action@github.com>
2026-01-20 11:48:14 +0000

    chore: update master script after merge [skip ci]

---
+++ RELEASE_SCRIPTS/Sprint160/DDL_CONSOLIDATED.sql
@@ -0,0 +1,31 @@
+-- BASELINE FROM ADHOC_SCRIPT.SQL --
+ALTER TABLE dbo.Employees 
+ALTER COLUMN LastName VARCHAR(100);
+
+
+ALTER TABLE dbo.Employees 
+ADD PhoneNumber VARCHAR(20);
+
+CREATE TABLE dbo.Departments (
+    DepartmentID INT PRIMARY KEY IDENTITY(1,1),
+    DeptName VARCHAR(100) NOT NULL,
+    Location VARCHAR(50)
+);
+
+
+CREATE TABLE STG_HIST_SCHEMA.Stg_Hist_Employees (
+    EmployeeID VARCHAR(20),                 
+    FirstName VARCHAR(100),
+    LastName VARCHAR(100),
+    Email VARCHAR(255),
+    Salary VARCHAR(50),                    
+    LastModifiedDate VARCHAR(50)
+  );
+
+
+GRANT EXECUTE ON dbo.CalculateBonuses TO HRUser;
+GRANT ALTER ON dbo.Departments TO LeadDeveloper;
+
+
+-- ####### MERGED CONTENT START #######
+

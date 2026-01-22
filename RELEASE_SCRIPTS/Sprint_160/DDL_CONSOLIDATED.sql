aca97049bcb477764a9d1ead882a1b9b8a0b59ae
GitHub Action <action@github.com>
2026-01-22 12:11:52 +0000

    chore: update DDL_CONSOLIDATED.sql after merge [skip ci]

--- RELEASE_SCRIPTS/Sprint_160/DDL_CONSOLIDATED.sql
+++ RELEASE_SCRIPTS/Sprint_160/DDL_CONSOLIDATED.sql
@@ -1,59 +1,16 @@
--- BASELINE FROM ADHOC_SCRIPT.SQL --
-ALTER TABLE dbo.Employees 
-ALTER COLUMN LastName VARCHAR(200);
-
-
-ALTER TABLE dbo.Employees 
-ADD PhoneNumber VARCHAR(20);
-
-CREATE TABLE dbo.Departments (
-    DepartmentID INT PRIMARY KEY IDENTITY(1,1),
-    DeptName VARCHAR(100) NOT NULL,
-    Location VARCHAR(50),
-    State Varchar (1000)
-);
-
-
-CREATE TABLE STG_HIST_SCHEMA.Stg_Hist_Employees (
-    EmployeeID VARCHAR(50),                 
-    FirstName VARCHAR(100),
-    LastName VARCHAR(100),
-    Email VARCHAR(255),
-    Salary VARCHAR(50),                    
-    LastModifiedDate VARCHAR(50)
-  );
-
-
-GRANT EXECUTE ON dbo.CalculateBonuses TO MainUser;
-GRANT ALTER ON dbo.Departments TO LeadDeveloper;
-
-
--- ####### MERGED CONTENT START #######
-
+-- START OF MASTER SCRIPT (No Adhoc Found) --
 --------------------------------------------------
 -- Source: BI_SCHEMA/sample_view1.sql
 --------------------------------------------------
 create or replace sampleView1
-select * from customers;
-
-
---------------------------------------------------
--- Source: BI_SCHEMA/script2.sql
---------------------------------------------------
-CREATE OR REPLACE VIEW department_salary_summary AS
-SELECT department, COUNT(employee_id) AS total_employees,
-    SUM(salary) AS total_budget,
-    ROUND(AVG(salary), 2) AS average_salary
-FROM 
-    employees
-GROUP BY 
-    department;
-
+select * from customers where date is not null;
 
 
 --------------------------------------------------
 -- Source: STG_SCHEMA/Stg_table1.sql
 --------------------------------------------------
-create or replace stg_schema.stg_table1();
+create or replace stg_schema.stg_table1(
+  id varchar(255)
+  );
 
 

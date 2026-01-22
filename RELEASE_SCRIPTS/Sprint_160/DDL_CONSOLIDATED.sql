3c65cb2672952a1262abccbed3f35cb122331068
GitHub Action <action@github.com>
2026-01-22 13:36:37 +0000

    chore: update DDL_CONSOLIDATED.sql after merge [skip ci]

--- RELEASE_SCRIPTS/Sprint_160/DDL_CONSOLIDATED.sql
+++ RELEASE_SCRIPTS/Sprint_160/DDL_CONSOLIDATED.sql
@@ -1,8 +1,1 @@
 -- START OF MASTER SCRIPT (No Adhoc Found) --
---------------------------------------------------
--- Source: BI_SCHEMA/View1.sql
---------------------------------------------------
-create or replace view1(
-  select * from table1 where id is null);
-
-

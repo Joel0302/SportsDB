b9e54979baca796c930b695db57b5d156595c09a
GitHub Action <action@github.com>
2026-01-22 13:26:36 +0000

    chore: update DDL_CONSOLIDATED.sql after merge [skip ci]

--- RELEASE_SCRIPTS/Sprint_160/DDL_CONSOLIDATED.sql
+++ RELEASE_SCRIPTS/Sprint_160/DDL_CONSOLIDATED.sql
@@ -1,16 +1,16 @@
 -- START OF MASTER SCRIPT (No Adhoc Found) --
 --------------------------------------------------
--- Source: BI_SCHEMA/sample_view1.sql
+-- Source: BI_SCHEMA/script2.sql
 --------------------------------------------------
-create or replace sampleView1
-select * from customers where date is not null;
+CREATE OR REPLACE VIEW department_salary_summary AS
+SELECT department, COUNT(employee_id) AS total_employees,
+    SUM(salary) AS total_budget,
+    ROUND(AVG(salary), 3) AS average_salary
+FROM 
+    employees
+GROUP BY 
+    department;
 
 
---------------------------------------------------
--- Source: STG_SCHEMA/Stg_table1.sql
---------------------------------------------------
-create or replace stg_schema.stg_table1(
-  id varchar(255)
-  );
 
 

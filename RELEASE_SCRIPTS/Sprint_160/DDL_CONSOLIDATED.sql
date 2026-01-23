38eb34ef7e371e61def3b074c20677574007d505
GitHub Action <action@github.com>
2026-01-23 13:39:40 +0000

    Update DDL_CONSOLIDATED.sql [skip ci]

--- RELEASE_SCRIPTS/Sprint_160/DDL_CONSOLIDATED.sql
+++ RELEASE_SCRIPTS/Sprint_160/DDL_CONSOLIDATED.sql
@@ -23,27 +23,5 @@ CREATE TABLE STG_HIST_SCHEMA.Stg_Hist_Employees (
     LastModifiedDate VARCHAR(50)
   );
 
-
--- ####### MERGED CONTENT START #######
-
---------------------------------------------------
--- Source: BI_SCHEMA/View1.sql
---------------------------------------------------
-create or replace view1(
-  select * from table1 where name is null);
-
-
---------------------------------------------------
--- Source: STG_SCHEMA/Stg_table1.sql
---------------------------------------------------
-create or replace stg_schema.stg_table1(
-  id varchar(255),
-  name varchar(255)
-  );
-
-
--- ####### GRANTS START ####### --
-GRANT EXECUTE ON dbo.CalculateBonuses TO HRUser;
-GRANT ALTER ON dbo.Departments TO LeadDeveloper;
-
+-- MERGED CONTENT START --
 

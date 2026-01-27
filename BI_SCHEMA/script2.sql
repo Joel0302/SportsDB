49ba584a87a3380f5b6ec23858fab7642a78640b
Joel Mario J <109203750+Joel0302@users.noreply.github.com>
2026-01-27 16:43:52 +0530

    Update script2.sql

--- BI_SCHEMA/script2.sql
+++ BI_SCHEMA/script2.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE VIEW department_salary_summary AS
 SELECT 
     department,
     COUNT(employee_id) AS total_employees,
-    SUM(salary) AS total_budget,
+    SUM(salary) AS totalbudget,
     ROUND(AVG(salary), 2) AS average_salary
 FROM 
     employees

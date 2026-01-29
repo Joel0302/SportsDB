1f2761de0b4a6c899d7bc51cc9ce9bbfa71377a4
Joel Mario J <109203750+Joel0302@users.noreply.github.com>
2026-01-29 16:43:33 +0530

    Update Histtable1.sql

--- STG_HIST_SCHEMA/Histtable1.sql
+++ STG_HIST_SCHEMA/Histtable1.sql
@@ -2,6 +2,6 @@ Create or replace histtable1(
   uid varchar(255),
   name varchar(255),
   Gender varchar(255),
-  Firstname varchar(255),
-  lastname varchar(255)
+  First_name varchar(255),
+  last_name varchar(255)
 );

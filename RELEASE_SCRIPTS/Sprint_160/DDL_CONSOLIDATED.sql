96079fdc039c2fc0f5015d88b62f083b079bd3a6
GitHub Action <action@github.com>
2026-01-29 10:43:19 +0000

    Update DDL_CONSOLIDATED.sql [skip ci]

--- RELEASE_SCRIPTS/Sprint_160/DDL_CONSOLIDATED.sql
+++ RELEASE_SCRIPTS/Sprint_160/DDL_CONSOLIDATED.sql
@@ -23,33 +23,6 @@ CREATE TABLE STG_HIST_SCHEMA.Stg_Hist_Employees (
     Salary VARCHAR(50),                    
     LastModifiedDate VARCHAR(50)
   );
-
--- MERGED CONTENT START --
-
--- Source: BI_SCHEMA/script2.sql --
-CREATE OR REPLACE VIEW department_salary_summary AS
-SELECT 
-    department,
-    COUNT(employee_id) AS totalemployees,
-    SUM(salary) AS totalbudget,
-    ROUND(AVG(salary), 2) AS averagesalary
-FROM 
-    employees
-GROUP BY 
-    department;
-
-
-
--- Source: STG_SCHEMA/Stg_table1.sql --
-create or replace stg_schema.stg_table1(
-  id varchar(255),
-  masterid varchar(255),
-  name varchar(255),
-  age varchar(1000),
-  gender varchar(255)
-  );
-
-
 -- GRANTS START --
 GRANT USAGE ON SCHEMA DBO_SCHEMA TO developer_role;
 

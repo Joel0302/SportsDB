8a70084896cfe871ba9da5b8556ad9c9eeed629b
Joel Mario J <109203750+Joel0302@users.noreply.github.com>
2026-01-29 16:42:25 +0530

    Update script2.sql

--- BI_SCHEMA/script2.sql
+++ BI_SCHEMA/script2.sql
@@ -3,9 +3,10 @@ SELECT
     department,
     COUNT(employee_id) AS totalemployees,
     SUM(salary) AS totalbudget,
-    ROUND(AVG(salary), 2) AS averagesalary
+    ROUND(AVG(salary), 3) AS averagesalary
 FROM 
     employees
 GROUP BY 
     department;
+
 

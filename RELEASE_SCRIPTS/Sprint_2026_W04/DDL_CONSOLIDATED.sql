234f198a38d787ed466eec7a4439d4b180173bad
GitHub Action <action@github.com>
2026-01-30 11:50:34 +0000

    Update DDL_CONSOLIDATED.sql [skip ci]

--- RELEASE_SCRIPTS/Sprint_2026_W04/DDL_CONSOLIDATED.sql
+++ RELEASE_SCRIPTS/Sprint_2026_W04/DDL_CONSOLIDATED.sql
@@ -1,18 +1,38 @@
--- START OF MASTER SCRIPT --
+-- BASELINE FROM ADHOC_SCRIPT.SQL --
+ALTER TABLE dbo.Employees 
+ALTER COLUMN LastName VARCHAR(200);
+
+
+ALTER TABLE dbo.Employees 
+ADD PhoneNumber VARCHAR(20);
+
+CREATE TABLE dbo.Departments (
+    DepartmentID INT PRIMARY KEY IDENTITY(1,1),
+    DeptName VARCHAR(100) NOT NULL,
+    Location VARCHAR(50),
+    State Varchar (1000),
+    country varchar(255)
+);
+
+
+CREATE TABLE STG_HIST_SCHEMA.Stg_Hist_Employees (
+    EmployeeID VARCHAR(50),                 
+    FirstName VARCHAR(100),
+    LastName VARCHAR(100),
+    Email VARCHAR(255),
+    Salary VARCHAR(50),                    
+    LastModifiedDate VARCHAR(50)
+  );
+
 
 -- MERGED CONTENT START --
-
--- Source: BI_WINGSPAN_ANALYTICS/TABLES/table.sql --
-Create or replace bi_schema.sampletable 
-id varchar(255) 
-  name varchar(255);
-
 
 -- Source: BI_WINGSPAN_ANALYTICS/VIEWS/VWSAMPLE.sql --
 
 CREATE VIEW BI_SCHEMA.v_Active_Users AS
 SELECT 
     UserID, 
+    USerAddress,
     UserName , 
     Email, 
     LastLoginDate
@@ -22,14 +42,9 @@ WHERE
     IsActive = 1;
 
 
--- Source: CERTIFIED_BUSINESS_WINGSPAN_ANALYTICS/VIEWS/VWADOBE.sql --
-Create or replace certifiedview.VWsample
-select 'mario' from source; 
-
-
 -- Source: STG_WINGSPAN_ANALYTICS/TABLES/SPORTS.sql --
 create or replace stg_schema.stg_table1(
-  id varchar(255),
+  id varchar(1000),
   masterid varchar(255),
   name varchar(255) ,
   age varchar(1000),
@@ -37,3 +52,7 @@ create or replace stg_schema.stg_table1(
   );
 
 
+-- GRANTS START --
+GRANT USAGE ON SCHEMA DBO_SCHEMA TO developer_role;
+
+GRANT CREATE ON SCHEMA DBO_SCHEMA TO senior_developer;

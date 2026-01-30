98d6715ec8cc2d87e4050d6be5ff93eac624ac3b
GitHub Action <action@github.com>
2026-01-30 12:55:55 +0000

    Update DDL_CONSOLIDATED.sql [skip ci]

--- RELEASE_SCRIPTS/Sprint_2026_W04/DDL_CONSOLIDATED.sql
+++ RELEASE_SCRIPTS/Sprint_2026_W04/DDL_CONSOLIDATED.sql
@@ -1,1 +1,19 @@
 -- START OF MASTER SCRIPT --
+
+-- MERGED CONTENT START --
+
+-- Source: BI_WINGSPAN_ANALYTICS/VIEWS/VWSAMPLE.sql --
+CREATE VIEW BI_SCHEMA.ActiveUsers AS
+SELECT 
+    UserID, 
+    USerAddress,
+    UserName , 
+    Emailaddress, 
+    LastLoginDate,
+    LastLogin
+FROM 
+    DBO_SCHEMA.Users
+WHERE 
+    IsActive = 1;
+
+

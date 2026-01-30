0a1913ae085ec7422d803ce9a998b346f395c7e5
GitHub Action <action@github.com>
2026-01-30 12:18:45 +0000

    Update DDL_CONSOLIDATED.sql [skip ci]

--- RELEASE_SCRIPTS/Sprint_2026_W04/DDL_CONSOLIDATED.sql
+++ RELEASE_SCRIPTS/Sprint_2026_W04/DDL_CONSOLIDATED.sql
@@ -27,13 +27,19 @@ CREATE TABLE STG_HIST_SCHEMA.Stg_Hist_Employees (
 
 -- MERGED CONTENT START --
 
+-- Source: BI_WINGSPAN_ANALYTICS/TABLES/table.sql --
+Create or replace bi_schema.sampletable 
+id varchar(255) ,
+  name varchar(255);
+
+
 -- Source: BI_WINGSPAN_ANALYTICS/VIEWS/VWSAMPLE.sql --
 CREATE VIEW BI_SCHEMA.ActiveUsers AS
 SELECT 
     UserID, 
     USerAddress,
     UserName , 
-    Email, 
+    Emailaddress, 
     LastLoginDate
 FROM 
     DBO_SCHEMA.Users
@@ -42,13 +48,14 @@ WHERE
 
 
 -- Source: CERTIFIED_BUSINESS_WINGSPAN_ANALYTICS/VIEWS/VWADOBE.sql --
-Create or replace certifiedview.VWsample(); 
+Create or replace certifiedview.VWsample()
+  select id,name from table1; 
 
 
 -- Source: STG_WINGSPAN_ANALYTICS/TABLES/SPORTS.sql --
 create or replace stg_schema.stg_table1(
   id varchar(1000),
-  masterid varchar(255),
+  masterid varchar(1000),
   name varchar(1000) ,
   age varchar(1000),
   Gender varchar(255)

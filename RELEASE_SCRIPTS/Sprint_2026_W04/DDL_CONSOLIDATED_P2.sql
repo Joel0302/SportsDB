84f99ca3bc2de01d9ece38325c819385cfadcb98
GitHub Action <action@github.com>
2026-02-02 07:43:24 +0000

    Auto-generated consolidated DDL phase script [skip ci]

---
+++ RELEASE_SCRIPTS/Sprint_2026_W04/DDL_CONSOLIDATED_P2.sql
@@ -0,0 +1,16 @@
+
+-- SECTION: FOLDER/FILE CHANGES --
+-- Source: BI_WINGSPAN_ANALYTICS/VIEWS/VWSAMPLE.sql --
+CREATE VIEW BI_SCHEMA.ActiveUsers AS
+SELECT 
+    UserIdentities , 
+    UserAddress,
+    UserName , 
+    Email, 
+    LoginDate
+FROM 
+    DBO_SCHEMA.Users
+WHERE 
+    IsActive = 1;
+
+

5c7a62840bb55a8342907d16c340a659ee8c17a9
Joel Mario J <109203750+Joel0302@users.noreply.github.com>
2026-03-03 17:56:59 +0530

    Delete RELEASE_SCRIPTS/Sprint_2026_W04/DDL_Scripts_2026-W04_V2.sql

--- RELEASE_SCRIPTS/Sprint_2026_W04/DDL_Scripts_2026-W04_V2.sql
+++
@@ -1,7 +0,0 @@
--- SECTION: ADHOC --
--- Source: RELEASE_SCRIPTS/Sprint_2026_W04/Adhoc_Scripts_Part02.sql --
-alter table tablename1
-  set columnnames=null;
-
-alter table tablename5
-  set columnname=instaid;

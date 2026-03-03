65864778694039e870e976bce6ba2d75838b1c2b
Joel Mario J <109203750+Joel0302@users.noreply.github.com>
2026-03-03 12:39:22 +0530

    Update Adhoc_Scripts.sql

--- RELEASE_SCRIPTS/Sprint_2026_W04/Adhoc_Scripts.sql
+++ RELEASE_SCRIPTS/Sprint_2026_W04/Adhoc_Scripts.sql
@@ -21,6 +21,6 @@ CREATE TABLE STG_HIST_SCHEMA.Stg_Hist_Employees (
     LastName VARCHAR(100),
     Email VARCHAR(255),
     Salary VARCHAR(50),                    
-    LastModifiedDate VARCHAR(50)
+    LastModifiedDate VARCHAR(150)
   );
 

25c3e2922447da242d6aea24d36793b8319ac18e
Joel Mario J <109203750+Joel0302@users.noreply.github.com>
2026-03-14 17:29:53 +0530

    Update histtable1.sql

--- STG_WINGSPAN_ANALYTICS_HIST/TABLES/histtable1.sql
+++ STG_WINGSPAN_ANALYTICS_HIST/TABLES/histtable1.sql
@@ -1,7 +1,7 @@
 Create or replace histtable1(
   uid varchar(255) ,
   names varchar(1000),
-  Gender varchar(1000),
+  GenderS varchar(1000),
   First_name varchar(255),
   last_name varchar(255),
   maidid varchar(255)

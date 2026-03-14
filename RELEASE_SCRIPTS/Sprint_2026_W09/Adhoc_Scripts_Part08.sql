8edf8aefd13cd2773275797c649119aed5fb5bac
Joel Mario J <109203750+Joel0302@users.noreply.github.com>
2026-03-14 17:42:10 +0530

    Create Adhoc_Scripts_Part08.sql

---
+++ RELEASE_SCRIPTS/Sprint_2026_W09/Adhoc_Scripts_Part08.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE STG_HIST_SCHEMA.Stg_Hist_Employees (
+    EmployeeID VARCHAR(50),                 
+    FirstName VARCHAR(100),
+    LastName VARCHAR(100),
+    Email VARCHAR(255),
+    Salary VARCHAR(50),                    
+    LastModifiedDate VARCHAR(150)
+  );
+

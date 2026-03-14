a3d87f235e343140dcee65643da983771cad348c
Joel Mario J <109203750+Joel0302@users.noreply.github.com>
2026-03-14 17:42:37 +0530

    Create Adhoc_Scripts_Part02.sql

---
+++ RELEASE_SCRIPTS/Sprint_2026_W09/Adhoc_Scripts_Part02.sql
@@ -0,0 +1,13 @@
+
+ALTER TABLE dbo.Employees 
+ALTER COLUMN FirstName VARCHAR(5000);
+
+CREATE TABLE STG_HIST_SCHEMA.Stg_Hist_Employees (
+    EmployeeID VARCHAR(50),                 
+    FirstName VARCHAR(100),
+    LastName VARCHAR(100),
+    Email VARCHAR(255),
+    Salary VARCHAR(50),                    
+    LastModifiedDate VARCHAR(150)
+  );
+

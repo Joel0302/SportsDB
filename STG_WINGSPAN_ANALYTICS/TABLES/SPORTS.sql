73b179ba7eeaf521beb0693739ab81fa6680797a
Joel Mario J <109203750+Joel0302@users.noreply.github.com>
2026-03-14 17:29:32 +0530

    Update SPORTS.sql

--- STG_WINGSPAN_ANALYTICS/TABLES/SPORTS.sql
+++ STG_WINGSPAN_ANALYTICS/TABLES/SPORTS.sql
@@ -1,6 +1,6 @@
 create or replace stg_schema.stg_table2(
   id varchar(1000),
-  masterNAME varchar(1000),
+  masterNAMES varchar(1000),
   names varchar(1000) ,
   age varchar(1000),
   Gender varchar(1000),

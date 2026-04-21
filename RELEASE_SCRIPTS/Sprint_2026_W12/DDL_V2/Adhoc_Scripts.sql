c6a144f7ffc47e27c525d2357ac7a60d0f9850bc
Joel Mario J <109203750+Joel0302@users.noreply.github.com>
2026-04-21 12:49:05 +0530

    Create Adhoc_Scripts.sql

---
+++ RELEASE_SCRIPTS/Sprint_2026_W12/DDL_V2/Adhoc_Scripts.sql
@@ -0,0 +1,10 @@
+Alter table tablename
+set column1=meta;
+
+CREATE TABLE dbo.bkp (
+    DepartmentID INT PRIMARY KEY IDENTITY(1,1),
+    DeptName VARCHAR(100) NOT NULL,
+    Location VARCHAR(50),
+    State Varchar (1000),
+    country varchar(255)
+);
